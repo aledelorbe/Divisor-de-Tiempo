e03fed131d1ace29a3e987964ee5ab71c7618fcc
aledelorbe <mgranadosb1800@alumno.ipn.mx>
2023-04-24 18:48:57 -0600

    se cambio el nombre de las variables de los elementos de la interfaz

--- app/src/main/java/com/example/divisortiempo/MainActivity.java
+++ app/src/main/java/com/example/divisortiempo/MainActivity.java
@@ -14,12 +14,12 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText tiempoEditText, bloquesEditText;
-    Button comenzarButton, detenerButton, reiniciarButton;
+    EditText edtTxtTiempo, edtTxtBloques;
+    Button btnComenzar, btnDetener, btnReiniciar;
     int tiempo, bloques, intervalo, intervaloOriginal, contadorBloques = 0, intervaloSeg;
     Timer timer, timer2, timer3;
     MediaPlayer mediaPlayer;
-    TextView minutosRestantes, segundosRestantes, txtBloques;
+    TextView txtMinutosRestantes, txtSegundosRestantes, txtBloques;
     boolean isTimerRunning = false, isTimerRunning2 = false, isTimerRunning3 = false;
 
     @Override
@@ -27,32 +27,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Edit Text
-        tiempoEditText = findViewById(R.id.tiempoEditText);
-        bloquesEditText = findViewById(R.id.bloquesEditText);
-        // Buttones
-        comenzarButton = findViewById(R.id.comenzarButton);
-        detenerButton = findViewById(R.id.detenerButton);
-        reiniciarButton = findViewById(R.id.reiniciarButton);
-
-        // Text View
-        minutosRestantes = findViewById(R.id.minutosRestantes);
-        segundosRestantes = findViewById(R.id.segundosRestantes);
+        edtTxtTiempo = findViewById(R.id.tiempoEditText);
+        edtTxtBloques = findViewById(R.id.bloquesEditText);
+        btnComenzar = findViewById(R.id.comenzarButton);
+        btnDetener = findViewById(R.id.detenerButton);
+        btnReiniciar = findViewById(R.id.reiniciarButton);
+        txtMinutosRestantes = findViewById(R.id.minutosRestantes);
+        txtSegundosRestantes = findViewById(R.id.segundosRestantes);
         txtBloques = findViewById(R.id.txtBloques);
 
         mediaPlayer = MediaPlayer.create(this, R.raw.alarm);
 
-        comenzarButton.setOnClickListener(this);
-        detenerButton.setOnClickListener(this);
-        reiniciarButton.setOnClickListener(this);
+        btnComenzar.setOnClickListener(this);
+        btnDetener.setOnClickListener(this);
+        btnReiniciar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view){
 
         if( view.getId() == R.id.comenzarButton ){
-            tiempo = Integer.parseInt(tiempoEditText.getText().toString());
-            bloques = Integer.parseInt(bloquesEditText.getText().toString());
+            tiempo = Integer.parseInt(edtTxtTiempo.getText().toString());
+            bloques = Integer.parseInt(edtTxtBloques.getText().toString());
             intervaloOriginal = intervalo = tiempo / bloques;
             //intervaloSeg = 59;
 
@@ -142,13 +138,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void tiempoMinRestante() {
-        minutosRestantes.setText( " " + (intervalo--) );
+        txtMinutosRestantes.setText( " " + (intervalo--) );
         if( intervalo == -1 ) // Porque cuando llegue al minuto 0, todavia quedarian 60 segundos mas.
             intervalo = intervaloOriginal;
     }
 
     public void tiempoSegRestante() {
-        segundosRestantes.setText( " " + (intervaloSeg--) );
+        txtSegundosRestantes.setText( " " + (intervaloSeg--) );
         if( intervaloSeg == -1 ) // Porque cuando llegue al minuto 0, todavia quedarian 60 segundos mas.
             intervaloSeg = 59;
     }
